52f809a208b81a3d1abfd495f886a5e705783776
massaroti <40370435+massaroti@users.noreply.github.com>
2019-08-06 10:36:07 -0300

    Add files via upload

---
+++ javaServer.java
@@ -0,0 +1,15 @@
+import java.net.*;
+class server
+    {
+        public static void main(String arg[])
+        {
+
+            try
+            {
+                ServerSocket server=new ServerSocket(3000);
+                Socket s=server.accept();
+
+                System.out.println("Connected");
+            }catch(Exception e){}
+        }
+    }
